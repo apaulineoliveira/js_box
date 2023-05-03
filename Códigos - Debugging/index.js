c959f37aa9f4838688e559e3541c6febfce691d4
apaulineoliveira <apaulineoliveira@gmail.com>
2023-05-03 01:28:21 -0300

    Códigos dos documentos/readmes

---
+++ Códigos - Debugging/index.js
@@ -0,0 +1,9 @@
+//Solução: Capturar nomes de variáveis e funções com erros ortográficos, corrija os dois erros de ortografia no código para que o cálculo netWorkingCapital funcione//
+
+// 'i' and 'e' trocados por "receivables" e faltando o 's' in "payables"//
+
+
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
